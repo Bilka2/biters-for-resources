76d780621727a99b7e25c855bca5817b334d367c
Bilka <Bilka2@users.noreply.github.com>
2021-04-17 19:07:13 +0200

    Added worms

--- data.lua
+++ data.lua
@@ -2,7 +2,7 @@ local util = require("util")
 local noise = require("noise")
 
 local function make_loot(item_name)
-  return {{item = item_name, count_min = 1, count_max = 3}}
+  return {{item = item_name, count_min = 1, count_max = 3}} -- TODO Bilka: vary by size?
 end
 
 local function resource_enemy_anim(enemyanimation, item_name, repeat_count)
@@ -47,7 +47,7 @@ local function resource_spitter_attack_anim(attackanimation, item_name)
   resource_enemy_anim(attackanimation, item_name, 28)
 end
 
-local function make_enemy_copy(map_color, item_name, size, type)
+local function make_unit_copy(map_color, item_name, size, type)
   local enemy = util.copy(data.raw.unit[size .. "-" .. type])
   enemy.localised_name = {"", {"entity-name." .. item_name}, " ", {"entity-name." .. enemy.name}}
   enemy.name = enemy.name .. ":" .. item_name
@@ -60,6 +60,36 @@ local function make_enemy_copy(map_color, item_name, size, type)
     resource_spitter_attack_anim(enemy.attack_parameters.animation, item_name)
   end
   data:extend{enemy}
+end
+
+-- See new_random_seed() in data\base\prototypes\entity\enemy-autoplace-utils.lua Line 130
+local count = 1
+local function make_worm_copy(resource, item_name, size)
+  local worm = util.copy(data.raw.turret[size .. "-worm-turret"])
+  worm.localised_name = {"", {"entity-name." .. item_name}, " ", {"entity-name." .. worm.name}}
+  worm.name = worm.name .. ":" .. item_name
+  worm.enemy_map_color = resource.map_color
+  worm.loot =  {{item = item_name, count_min = 25, count_max = 75}} -- TODO Bilka: this correct? vary by size?
+
+  -- data\base\prototypes\entity\enemy-autoplace-utils.lua Line 146
+  local distance_height_multiplier = worm.autoplace.probability_expression.arguments.source.arguments[1].arguments[2]
+  -- data\base\prototypes\entity\enemy-autoplace-utils.lua Line 151 (0.25 + distance_factor * 0.05)
+  local distance_limiter = worm.autoplace.probability_expression.arguments.source.arguments[3]
+
+  worm.autoplace = util.copy(resource.autoplace)
+  worm.autoplace.force = "enemy"
+  worm.autoplace.order = "z" .. worm.autoplace.order
+  worm.autoplace.probability_expression = noise.min(worm.autoplace.probability_expression * distance_height_multiplier, distance_limiter)
+  count = count + 1
+  worm.autoplace.probability_expression = noise.random_penalty(worm.autoplace.probability_expression, 0.1, {
+    x = noise.var("x") + count -- See new_random_seed() in data\base\prototypes\entity\enemy-autoplace-utils.lua Line 130
+  })
+
+  worm.spawn_decoration[2].decorative = "shroom-decal:" .. item_name
+  worm.spawn_decoration[3].decorative = "enemy-decal:" .. item_name
+  worm.spawn_decoration[4].decorative = "enemy-decal-transparent:" .. item_name
+
+  data:extend{worm}
 end
 
 local function resource_enemy_decals(map_color, item_name)
@@ -187,6 +217,269 @@ local function resource_enemy_decals(map_color, item_name)
     }
   }
 
+  local base_decorative_sprite_priority = "extra-high"
+  local shroom_decal = util.copy(data.raw["optimized-decorative"]["shroom-decal"])
+  shroom_decal.name = shroom_decal.name .. ":" .. item_name
+  shroom_decal.pictures =
+  {
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-00.png",
+      priority = base_decorative_sprite_priority,
+      width = 166,
+      height = 88,
+      shift = util.by_pixel(-12, -10),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-00.png",
+        priority = base_decorative_sprite_priority,
+        width = 334,
+        height = 206,
+        shift = util.by_pixel(-13, -1),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-01.png",
+      priority = base_decorative_sprite_priority,
+      width = 128,
+      height = 94,
+      shift = util.by_pixel(12, 6),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-01.png",
+        priority = base_decorative_sprite_priority,
+        width = 256,
+        height = 182,
+        shift = util.by_pixel(12, 7),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-02.png",
+      priority = base_decorative_sprite_priority,
+      width = 204,
+      height = 96,
+      shift = util.by_pixel(-8, -2),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-02.png",
+        priority = base_decorative_sprite_priority,
+        width = 406,
+        height = 194,
+        shift = util.by_pixel(-8, -3),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-03.png",
+      priority = base_decorative_sprite_priority,
+      width = 216,
+      height = 96,
+      shift = util.by_pixel(6, 8),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-03.png",
+        priority = base_decorative_sprite_priority,
+        width = 432,
+        height = 220,
+        shift = util.by_pixel(6, 1),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-04.png",
+      priority = base_decorative_sprite_priority,
+      width = 184,
+      height = 102,
+      shift = util.by_pixel(-12, 8),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-04.png",
+        priority = base_decorative_sprite_priority,
+        width = 368,
+        height = 206,
+        shift = util.by_pixel(-12, 7),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-05.png",
+      priority = base_decorative_sprite_priority,
+      width = 170,
+      height = 102,
+      shift = util.by_pixel(24, 2),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-05.png",
+        priority = base_decorative_sprite_priority,
+        width = 340,
+        height = 200,
+        shift = util.by_pixel(24, 3),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-06.png",
+      priority = base_decorative_sprite_priority,
+      width = 162,
+      height = 108,
+      shift = util.by_pixel(24, 2),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-06.png",
+        priority = base_decorative_sprite_priority,
+        width = 326,
+        height = 214,
+        shift = util.by_pixel(23, 2),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-07.png",
+      priority = base_decorative_sprite_priority,
+      width = 168,
+      height = 98,
+      shift = util.by_pixel(20, 8),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-07.png",
+        priority = base_decorative_sprite_priority,
+        width = 336,
+        height = 190,
+        shift = util.by_pixel(20, 9),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-08.png",
+      priority = base_decorative_sprite_priority,
+      width = 192,
+      height = 104,
+      shift = util.by_pixel(-12, 0),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-08.png",
+        priority = base_decorative_sprite_priority,
+        width = 386,
+        height = 206,
+        shift = util.by_pixel(-12, 1),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-09.png",
+      priority = base_decorative_sprite_priority,
+      width = 138,
+      height = 78,
+      shift = util.by_pixel(8, -12),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-09.png",
+        priority = base_decorative_sprite_priority,
+        width = 278,
+        height = 150,
+        shift = util.by_pixel(8, -11),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-10.png",
+      priority = base_decorative_sprite_priority,
+      width = 182,
+      height = 100,
+      shift = util.by_pixel(2, -2),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-10.png",
+        priority = base_decorative_sprite_priority,
+        width = 364,
+        height = 204,
+        shift = util.by_pixel(2, -3),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-11.png",
+      priority = base_decorative_sprite_priority,
+      width = 192,
+      height = 104,
+      shift = util.by_pixel(22, 4),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-11.png",
+        priority = base_decorative_sprite_priority,
+        width = 378,
+        height = 206,
+        shift = util.by_pixel(23, 5),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-12.png",
+      priority = base_decorative_sprite_priority,
+      width = 160,
+      height = 108,
+      shift = util.by_pixel(-4, 4),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-12.png",
+        priority = base_decorative_sprite_priority,
+        width = 320,
+        height = 220,
+        shift = util.by_pixel(-4, 3),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-13.png",
+      priority = base_decorative_sprite_priority,
+      width = 158,
+      height = 82,
+      shift = util.by_pixel(32, 10),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-13.png",
+        priority = base_decorative_sprite_priority,
+        width = 318,
+        height = 160,
+        shift = util.by_pixel(31, 11),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-14.png",
+      priority = base_decorative_sprite_priority,
+      width = 186,
+      height = 104,
+      shift = util.by_pixel(24, 6),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-14.png",
+        priority = base_decorative_sprite_priority,
+        width = 368,
+        height = 206,
+        shift = util.by_pixel(25, 6),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__biters-for-resources__/graphics/shroom-decal/shroom-decal-15.png",
+      priority = base_decorative_sprite_priority,
+      width = 208,
+      height = 112,
+      shift = util.by_pixel(-6, 0),
+      hr_version =
+      {
+        filename = "__biters-for-resources__/graphics/shroom-decal/hr-shroom-decal-15.png",
+        priority = base_decorative_sprite_priority,
+        width = 410,
+        height = 222,
+        shift = util.by_pixel(-5, 0),
+        scale = 0.5
+      }
+    }
+  }
+
   local tint = util.copy(map_color)
   if item_name == "coal" then
     tint = {0.2, 0.2, 0.2}
@@ -198,9 +491,14 @@ local function resource_enemy_decals(map_color, item_name)
   for _, v in pairs(enemy_decal_t.pictures) do
     v.tint = tint
   end
+  for _, v in pairs(shroom_decal.pictures) do
+    v.tint = tint
+    v.hr_version.tint = tint
+  end
 
   data:extend{enemy_decal}
   data:extend{enemy_decal_t}
+  data:extend{shroom_decal}
 end
 
 local function resource_spawner_anim(spawner_idle_animation, item_name)
@@ -225,14 +523,12 @@ local function resource_spawner_anim(spawner_idle_animation, item_name)
   end
 end
 
--- See new_random_seed() in data\base\prototypes\entity\enemy-autoplace-utils.lua Line 130
-local count = 1
 local function make_resource_enemies(resource, item_name, enemy_type)
   local map_color = resource.map_color
-  make_enemy_copy(map_color, item_name, "small", enemy_type)
-  make_enemy_copy(map_color, item_name, "medium", enemy_type)
-  make_enemy_copy(map_color, item_name, "big", enemy_type)
-  make_enemy_copy(map_color, item_name, "behemoth", enemy_type)
+  make_unit_copy(map_color, item_name, "small", enemy_type)
+  make_unit_copy(map_color, item_name, "medium", enemy_type)
+  make_unit_copy(map_color, item_name, "big", enemy_type)
+  make_unit_copy(map_color, item_name, "behemoth", enemy_type)
 
   local spawner = util.copy(data.raw["unit-spawner"][enemy_type .. "-spawner"])
   spawner.localised_name = {"", {"entity-name." .. item_name}, " ", {"entity-name." .. spawner.name}}
@@ -250,7 +546,7 @@ local function make_resource_enemies(resource, item_name, enemy_type)
   spawner.autoplace.probability_expression = spawner.autoplace.probability_expression * 0.25
   count = count + 1
   spawner.autoplace.probability_expression = noise.random_penalty(spawner.autoplace.probability_expression, 0.1, {
-    x = noise.var("x") + count
+    x = noise.var("x") + count -- See new_random_seed() in data\base\prototypes\entity\enemy-autoplace-utils.lua Line 130
   })
 
   spawner.spawn_decoration[3].decorative = "enemy-decal:" .. item_name
@@ -268,6 +564,10 @@ local function enemies_for_resource(resource_name)
   local resource = util.copy(data.raw.resource[resource_name])
   local item_name = resource.minable.result
   resource_enemy_decals(resource.map_color, item_name)
+  make_worm_copy(resource, item_name, "small")
+  make_worm_copy(resource, item_name, "medium")
+  make_worm_copy(resource, item_name, "big")
+  make_worm_copy(resource, item_name, "behemoth")
   make_resource_enemies(resource, item_name, "biter")
   make_resource_enemies(resource, item_name, "spitter")
 
--- todo.md
+++ todo.md
@@ -1,7 +1,7 @@
 * [x] make ore biters
 * [x] take autoplace from ores, make biter spawners
 * [x] add spitters + spitter spawners, same as above
-* [ ] add worms
+* [x] add worms
 * [x] add drops
 * [x] add different drop ratios
 * [x] add locale
@@ -10,4 +10,4 @@
 * [ ] drop oil barrels that do not result in barrels when emptied
 * [x] decide how to handle uranium
 * [x] just ignore the acid requirement for uranium 
-* [x] balancing
+* [ ] balancing
