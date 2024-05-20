7a4e54a38b51ed0bad8d7101667100c36ebc0a8d
Bilka <Bilka2@users.noreply.github.com>
2024-05-20 13:29:08 +0200

    Added executable permissions to release script files

--- scripts/extract_changes.sh
+++ scripts/extract_changes.sh
@@ -3,4 +3,4 @@
 #
 # ignore first 3 lines and match until line starts with ----, remove said line, remove two spaces at start of all lines,
 #  add linebreak in front of line if it starts with a non-space but ignore the first line
-sed -n -e '4,/^----/p' changelog.txt | sed -e '/^----/d' | sed 's/^\s\s//g' | sed -E '2,$s/^(\S)/\n\1/g' >> changes.txt 
+sed -n -e '4,/^----/p' changelog.txt | sed -e '/^----/d' | sed 's/^\s\s//g' | sed -E '2,$s/^(\S)/\n\1/g' >> changes.txt
